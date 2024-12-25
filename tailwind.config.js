965627015b83541f122b2b39c2eb34e18ff7fe2b
sandeep1555 <sandeepdasari614@gmail.com>
2024-12-25 17:02:41 +0530

    import daisyui changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
 export default {
   content: [
     "./index.html",
@@ -11,7 +12,7 @@ export default {
     themes: ["wireframe"],
   },
   plugins: [
-    require('daisyui'),
+    daisyui,
   ],
 };
 
