867b2345dc34baa62f68b2fb2269597cf367614c
sandeep1555 <sandeepdasari614@gmail.com>
2024-12-25 16:58:21 +0530

    tailwind.config file changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -14,3 +14,4 @@ module.exports = {
     require('daisyui'),
   ],
 };
+
