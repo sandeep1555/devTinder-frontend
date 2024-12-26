a2002a2c9697e3ea08f48b52eda3ab68080311c0
sandeep1555 <sandeepdasari614@gmail.com>
2024-12-26 23:46:50 +0530

    footer css changed

--- src/Body.jsx
+++ src/Body.jsx
@@ -37,9 +37,11 @@ const Body = () => {
 
   }, [])
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <NavBar />
-      <Outlet />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
       <Footer />
     </div>
   )
