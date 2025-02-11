3888e1e7d4eb1b056999a6556218808b8c0284e6
sandeep1555 <sandeepdasari614@gmail.com>
2025-02-11 15:54:55 +0530

    socket config for production

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -3,5 +3,12 @@ import { BASE_URL } from "./constants"
 
 export const createSocketConnection=()=>
 {
-    return io(BASE_URL)
+    if(location.hostname==="loclahost")
+    {
+        return io(BASE_URL)
+    }
+    else{
+        return io("/",{path:"/api/socket.io"})
+    }
+
 }
